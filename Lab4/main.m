13af543237a793e24f5760bb2b69d301eeb8134b
Yurie <yurie.koga.a@gmail.com>
2021-02-28 10:56:30 -0800

    changed if to switch

--- Lab4/main.m
+++ Lab4/main.m
@@ -22,22 +22,46 @@ int main(int argc, const char * argv[]) {
             prompt = [prompt stringByAppendingString: str];
         }
         
-        while (YES) {
+        bool isContinue = YES;
+        while (isContinue) {
             NSString *strInput = [InputHandler getUserInputWithMax: 255 AndPrompt: prompt];
             NSInteger item = [menus indexOfObject: strInput];
             
-            if(item == 0) {
-                NSString *inputName = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput name: "];
-                NSString *inputEmail = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput email: "];
-                Contact *person1 = [[Contact alloc] initWithName:inputName AndWithEmail:inputEmail];
-                [contacts addContact:person1];
-            } else if (item == 1) {
-                [contacts displayContacts];
-            } else if (item == 2) {
-                printf("Application will be terminated....\n");
-                break;
-            } else {
-                printf("input is invalid\n");
+//            if(item == 0) {
+//                NSString *inputName = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput name: "];
+//                NSString *inputEmail = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput email: "];
+//                Contact *person1 = [[Contact alloc] initWithName:inputName AndWithEmail:inputEmail];
+//                [contacts addContact:person1];
+//            } else if (item == 1) {
+//                [contacts displayContacts];
+//            } else if (item == 2) {
+//                printf("Application will be terminated....\n");
+//                break;
+//            } else {
+//                printf("input is invalid\n");
+//            }
+            
+            // 1. switch can handle only integer (also char shince it's integer)
+            // 2. need a block '{}' when want to declare variables inside cases
+            switch (item) {
+                case 0:;
+                {
+                    NSString *inputName = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput name: "];
+                    NSString *inputEmail = [InputHandler getUserInputWithMax: 255 AndPrompt: @"\ninput email: "];
+                    Contact *person1 = [[Contact alloc] initWithName:inputName AndWithEmail:inputEmail];
+                    [contacts addContact:person1];
+                    break;
+                }
+                case 1:
+                    [contacts displayContacts];
+                    break;
+                case 2:
+                    printf("Application will be terminated....\n");
+                    isContinue = NO;
+                    break;
+                default:
+                    printf("input is invalid\n");
+                    break;
             }
         }
     }
